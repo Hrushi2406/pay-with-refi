15e006a3716fa2bc379536b939c767b2126ded87
hrushikesh <kukularehrushi@gmail.com>
2022-04-01 10:12:24 +0530

    Initial Setup

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'pay-with-refi'
+import { PayWithRefi } from 'pay-with-refi'
 import 'pay-with-refi/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <PayWithRefi text='Create React Library Example 😄' />
 }
 
 export default App
